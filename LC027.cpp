eb6e290f453d59a62f8bd4b170804b1fd8152951
ma1371 <ma.1371@osu.edu>
2016-10-22 16:33:38 -0400

    Revert "fight"
    This reverts commit 90e80f7ecd2c49c3bb29b83d336ddc043f2cb581.

--- LC027.cpp
+++
@@ -1,6 +0,0 @@
-class Solution {
-public:
-	int removeElement(vector<int>& nums, int val) {
-		return remove(nums.begin(), nums.end(), val) - nums.begin();
-	}
-};
--- LC258.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-	int addDigits(int num) {
-		if (num<10)
-			return num;
-		int ans = (num - 10) % 9 + 1;
-		return ans;
-	}
-};
